b449731fc5da55f7eb43d1f1d63c1e1ba7b6d2eb
Mohammed Code <54428815+Mohammedorax@users.noreply.github.com>
2020-08-07 01:42:22 +0300

    Add files via upload

---
+++ helloworld/bin/main.dart
@@ -0,0 +1,121 @@
+main(List<String> arguments) {
+
+/*....................................
+var a = 10;
+bool b = true;
+if (b) {
+  print( 'a > 5' );
+}else{
+  print('a not > 5');
+}
+//....................................
+var a = 10;
+bool b = true;
+bool c = !b;
+//if ( ! (a > 5)){}
+if (a !=10) {
+  print('Yes');
+}else{
+  print('No');
+}
+//....................................
+//for (var i = 0; i < 10; i= i+1 ){}
+for (var i = 0; i < 10; i++) {
+  print(i);
+}
+//....................................
+var a = 6;
+for(int i = 0; i < 10; i++){
+ //a = a + 5;
+  a += 5;
+  print(a);
+}
+//....................................
+var a = 10;
+var b = a +25;
+a = b - 4;
+
+if (a==10){
+ print("a == 10");
+}
+if (a == 31){
+ print("a == 15");
+}
+if (a==25) {
+  print('a==25');
+}
+//....................................
+*/
+
+var a = 2 ;
+switch (a){
+  case 1:
+  print('Yes, it is equal= 1');
+  break;
+  case 2:
+  print('Yes, it is equal= 2');
+  break;
+  case 3:
+  print('Yes, it is equal= 3');
+  break;
+  case 4:
+  print('Yes, it is equal= 4');
+  break;
+}
+
+
+
+
+
+
+
+
+
+
+
+var name= 'Mohammed';
+var salary= 2500;
+print(name);
+print(salary);
+// او 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//switch('Code Mohammed'){
+//case'Code Mohammed':
+//print('Code Mohammed');
+//}
+//
+////Code Mohammed
+//var m ='Code Mohammed';
+//print(m);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
